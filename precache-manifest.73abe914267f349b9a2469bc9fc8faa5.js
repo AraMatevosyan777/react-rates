27229a02abadd05bca41a84655f4cfa3166db07a
Aro Matevosyan <AroMatevosyan777@gmil.com>
2020-04-29 17:06:54 +0400

    Updates

---
+++ precache-manifest.73abe914267f349b9a2469bc9fc8faa5.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f4669bc142d5efeff74d09a967e656f",
+    "url": "/react-rates/index.html"
+  },
+  {
+    "revision": "0af9ca52c7272ffa4124",
+    "url": "/react-rates/static/css/2.12c4a0b7.chunk.css"
+  },
+  {
+    "revision": "595bd28ed6d4827e0d69",
+    "url": "/react-rates/static/css/main.706b97c7.chunk.css"
+  },
+  {
+    "revision": "0af9ca52c7272ffa4124",
+    "url": "/react-rates/static/js/2.17a7c02e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-rates/static/js/2.17a7c02e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "595bd28ed6d4827e0d69",
+    "url": "/react-rates/static/js/main.8464b39c.chunk.js"
+  },
+  {
+    "revision": "a2702081542f55eac4a2",
+    "url": "/react-rates/static/js/runtime-main.3fcb7772.js"
+  },
+  {
+    "revision": "05841c4c94612101e37f4ca04510354f",
+    "url": "/react-rates/static/media/loader.05841c4c.svg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/react-rates/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/react-rates/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/react-rates/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/react-rates/static/media/slick.f97e3bbf.svg"
+  }
+]);
