0a180b159b70e618bd0c7b175f631b961d40cc07
Aro Matevosyan <AroMatevosyan777@gmil.com>
2020-04-29 17:17:41 +0400

    deploy my app to gh-pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Navbar from './components/Navbar/Navbar';
-import { Route } from 'react-router-dom';
+import { Route, Redirect } from 'react-router-dom';
 import HomeContainer from './components/Home/HomePage/HomeContainer';
 import Footer from './components/Footer/Footer';
 import About from './components/About/About';
@@ -10,6 +10,7 @@ function App() {
     <div >
       <Navbar/>
       <div className='container min-vh-100'>
+        <Route path='/' render={ ()=> <Redirect to={"/home"}/>} />
         <Route path='/home' render={()=> <HomeContainer/>}/>
         <Route path='/something' render={()=> <About/>}/>
       </div>
